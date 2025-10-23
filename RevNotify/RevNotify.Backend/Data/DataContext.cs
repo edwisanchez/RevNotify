0b8c6997ed254cea69ac517e5ef040b66f2da596
Edwin <eedsc99@gmail.com>
2025-10-23 18:25:53 -0500

    changes

---
+++ RevNotify/RevNotify.Backend/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RevNotify.Shared.Entites;
+
+namespace RevNotify.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ RevNotify/RevNotify.Shared/Entites/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RevNotify.Shared.Entites
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+        public string City { get; set; } = null!;
+
+        [Required]
+        public string Telephone { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+    }
+}
